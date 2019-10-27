f4a6bcfc6a3f6c53ad6a2f038a17933dad9f6b00
Dara Oladosu <daradosu@gmail.com>
2019-10-27 17:54:33 +0100

    prep for testing

---
+++ test/index.js
@@ -0,0 +1,16 @@
+var assert = require('assert');
+var log = console.log;
+
+describe('tests', function() {
+  describe('first dummy test', function (){
+    it('should run first dummy test', function() {
+      assert(true);
+    });
+  });
+
+  describe('second dummy test', function (){
+    it('should run second dummy test', function() {
+      assert(true);
+    });
+  });
+});
